9baef1d13348ab7fb6a01ad4b9e8ba6668044042
Santu pramanik <santu700141@gmail.com>
2025-10-24 00:44:46 +0530

    Design the Chatbox section

--- frontend/src/components/Chatbox.jsx
+++ frontend/src/components/Chatbox.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {useEffect, useState} from "react";
+import {useAppContext} from "../context/Appcontext";
+import {assets} from "../assets/assets";
+import Message from "./Message";
 
 const Chatbox = () => {
-  return (
-    <div>Chatbox</div>
-  )
-}
+    const {selectedChat, theme} = useAppContext();
 
-export default Chatbox
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // whenever user select the messages it will display here
+    useEffect(() => {
+        if (selectedChat) {
+            setMessages(selectedChat.messages);
+        }
+    }, [selectedChat]);
+
+    return (
+        <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
+            {/* Chat messages */}
+            <div className="flex-1 mb-5 overflow-y-scroll">
+                {messages.length == 0 && (
+                    <div className="flex flex-col items-center justify-center h-full gap-2 text-primary">
+                        <img
+                            src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+                            className="w-full max-w-56 sm:max-w-68"
+                            alt="image"
+                        ></img>
+                        <p
+                            className={`mt-5 text-4xl sm:text-6xl text-center text-gray-400  ${
+                                theme === "dark" ? "text-white" : ""
+                            }`}
+                        >
+                            Ask me anything.
+                        </p>
+                    </div>
+                )}
+                {messages.map((message, index) => (
+                    <Message key={index} message={message}></Message>
+                ))}
+            </div>
+
+            {/* Prompt input box */}
+            <form></form>
+        </div>
+    );
+};
+
+export default Chatbox;
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import {assets} from "../assets/assets";
+import {useAppContext} from "../context/Appcontext";
+import moment from "moment";
 
-const Message = () => {
-  return (
-    <div>Message</div>
-  )
-}
+const Message = ({message}) => {
+    const {theme} = useAppContext();
+    return (
+        <div>
+            {message.role === "user" ? (
+                <div className="flex items-start justify-end my-4 gap-2">
+                    <div
+                        className={`flex flex-col gap-2  p-2 px-4 bg-slate-50 ${
+                            theme === "dark" ? "bg-[#57317C]/30 border border-[#80609F] " : ""
+                        } rounded-md max-w-2xl`}
+                    >
+                        <p className={`text-sm  ${theme === "dark" ? "text-primary" : ""}`}>{message.content}</p>
+                        <span className={`text-xs text-gray-400 ${theme == "dark" ? "text-[#B1A67C0]" : ""}`}>
+                            {moment(message.timestamp).fromNow()}
+                        </span>
+                    </div>
+                    <img src={assets.user_icon} className="w-8 rounded-full"></img>
+                </div>
+            ) : (
+                <div
+                    className={`inline-flex flex-col gap-2 p-2 px-4 max-w-2xl bg-primary/20 ${
+                        theme === "dark" ? "bg-[#57317C]/30 border border-[#80609F] " : ""
+                    } rounded-md my-4 `}
+                >
+                    {message.isImage ? (
+                        <img src={message.content} className="w-full max-w-md mt-2 rounded-md"></img>
+                    ) : (
+                        <div className={`text-sm ${theme == "dark" ? "text-primary" : ""} reset-tw`}>
+                            {message.content}
+                        </div>
+                    )}
+                      <span className={`text-xs text-gray-400 ${theme == "dark" ? "text-[#B1A67C0]" : ""}`}>
+                            {moment(message.timestamp).fromNow()}
+                        </span>
+                </div>
+            )}
+        </div>
+    );
+};
 
-export default Message
+export default Message;
